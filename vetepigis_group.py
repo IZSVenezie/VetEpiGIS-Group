3d4a0bb1a415ca970d4bc25c9343110cb9fb9093
solymosin <solymosi.norbert@gmail.com>
2016-05-21 06:54:21 +0200

    0.1

---
+++ vetepigis_group.py
@@ -0,0 +1,166 @@
+# -*- coding: utf-8 -*-
+"""
+/***************************************************************************
+ QVetGroup
+   A QGIS plugin
+   Spatial functions for vet epidemiology
+                              -------------------
+        begin                : 2016-05-06
+        git sha              : $Format:%H$
+        copyright            : (C) 2016 by Norbert Solymosi
+        email                : solymosi.norbert@gmail.com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+"""
+
+import os, shutil
+from PyQt4.QtGui import *
+from PyQt4.QtCore import SIGNAL, Qt, QSettings, QCoreApplication, QFile, QFileInfo, QDate, QVariant, \
+    pyqtSignal, QRegExp, QDateTime, QTranslator, QSize
+from PyQt4.QtSql import *
+from PyQt4.QtXml import *
+
+from qgis.core import QgsField, QgsSpatialIndex, QgsMessageLog, QgsProject, \
+    QgsCoordinateTransform, QGis, QgsVectorFileWriter, QgsMapLayerRegistry, QgsFeature, \
+    QgsGeometry, QgsFeatureRequest, QgsPoint, QgsVectorLayer, QgsCoordinateReferenceSystem, \
+    QgsRectangle, QgsDataSourceURI, QgsDataProvider, QgsComposition, QgsComposerMap, QgsAtlasComposition
+from qgis.gui import QgsMapTool, QgsMapToolEmitPoint, QgsMessageBar, QgsRubberBand
+
+from plugin import xabout
+import resources_rc
+
+
+from uuid import getnode as get_mac
+
+class QVetGroup:
+    """QGIS Plugin Implementation."""
+
+    def __init__(self, iface):
+        """Constructor.
+
+        :param iface: An interface instance that will be passed to this class
+        :param iface: An interface instance that will be passed to this class
+            which provides the hook by which you can manipulate the QGIS
+            application at run time.
+        :type iface: QgsInterface
+        """
+        # Save reference to the QGIS interface
+        self.iface = iface
+
+        # initialize plugin directory
+        self.plugin_dir = os.path.dirname(__file__)
+        # initialize locale
+        # self.loadSettings()
+
+        locale = QSettings().value('locale/userLocale')[0:2]
+        locale_path = os.path.join(
+            self.plugin_dir,
+            'i18n',
+            'QVetGroup_{}.qm'.format(locale))
+
+        self.vers = '0.1'
+        self.prevcur = self.iface.mapCanvas().cursor()
+
+        self.origtool = QgsMapTool(self.iface.mapCanvas())
+
+        if os.path.exists(locale_path):
+            self.translator = QTranslator()
+            self.translator.load(locale_path)
+
+            if qVersion() > '4.3.3':
+                QCoreApplication.installTranslator(self.translator)
+
+        mac = '_'.join(("%012X" % get_mac())[i:i+2] for i in range(0, 12, 2))
+        dbuid = 'db_%s.sqlite' % mac
+        dbfold = os.path.join(self.plugin_dir, 'db')
+        dbuidpath = os.path.join(dbfold, dbuid)
+        if not os.path.isfile(dbuidpath):
+            shutil.copy(os.path.join(dbfold, 'base.sqlite'), dbuidpath)
+
+        self.uri = QgsDataSourceURI()
+        self.uri.setDatabase(dbuidpath)
+
+        self.db = QSqlDatabase.addDatabase('QSPATIALITE')
+        self.db.setDatabaseName(self.uri.database())
+
+
+    # noinspection PyMethodMayBeStatic
+    def tr(self, message):
+        """Get the translation for a string using Qt translation API.
+
+        We implement this ourselves since we do not inherit QObject.
+
+        :param message: String for translation.
+        :type message: str, QString
+
+        :returns: Translated version of message.
+        :rtype: QString
+        """
+        # noinspection PyTypeChecker,PyArgumentList,PyCallByClass
+        return QCoreApplication.translate('QVetGroup', message)
+
+
+    def initGui(self):
+        """Create the menu entries and toolbar icons inside the QGIS GUI."""
+
+        self.actAbout = QAction(
+            QIcon(':/plugins/QVetGroup/images/icon02.png'),
+            QCoreApplication.translate('QVetGroup', 'About'),
+            self.iface.mainWindow())
+        self.iface.addPluginToMenu('&QVetGroup', self.actAbout)
+        self.actAbout.triggered.connect(self.about)
+
+        self.toolbar = self.iface.addToolBar(
+            QCoreApplication.translate('QVetGroup', 'QVetGroup'))
+        self.toolbar.setObjectName(
+            QCoreApplication.translate('QVetGroup', 'QVetGroup'))
+
+        """Add buttons to the toolbar"""
+
+
+
+    def unload(self):
+        """Removes the plugin menu item and icon from QGIS GUI."""
+        self.iface.removePluginMenu('&QVetGroup', self.actAbout)
+        del self.toolbar
+
+
+    def about(self):
+        dlg = xabout.Dialog()
+        dlg.setWindowTitle('About')
+        dlg.label.setPixmap(QPixmap(':/plugins/QVetGroup/images/QVetGroup-about-banner.png'))
+        ow = dlg.textEdit.fontWeight()
+
+        dlg.textEdit.setFontWeight(QFont.Bold)
+        dlg.textEdit.append('QVetGroup ' + self.vers +'\n')
+        dlg.textEdit.setFontWeight(ow)
+        dlg.textEdit.append('QVetGroup is a free QGIS plugin helping veterinarians in the management of spatial data related to animal disease.\n')
+        dlg.textEdit.setFontWeight(QFont.Bold)
+        dlg.textEdit.append('Developers:')
+        dlg.textEdit.setFontWeight(ow)
+        dlg.textEdit.append('Norbert Solymosi *;\n* from University of Veterinary Medicine, Budapest.\n')
+        dlg.textEdit.setFontWeight(QFont.Bold)
+        dlg.textEdit.append('Contributors:')
+        dlg.textEdit.setFontWeight(ow)
+        dlg.textEdit.append(u'Nicola Ferrè *;\nMatteo Mazzucato *;\n* from Istituto Zooprofilattico Sperimentale delle Venezie.\n')
+        dlg.textEdit.setFontWeight(QFont.Bold)
+        dlg.textEdit.append('Contacts:')
+        dlg.textEdit.setFontWeight(ow)
+        dlg.textEdit.append('Send an email to gis@izsvenezie.it\n\n')
+        dlg.textEdit.append('Original icons designed by Feepik. They were modified for this project by IZSVe.')
+        dlg.textEdit.moveCursor(QTextCursor.Start, QTextCursor.MoveAnchor)
+        dlg.textEdit.setReadOnly(True)
+
+        x = (self.iface.mainWindow().x()+self.iface.mainWindow().width()/2)-dlg.width()/2
+        y = (self.iface.mainWindow().y()+self.iface.mainWindow().height()/2)-dlg.height()/2
+        dlg.move(x,y)
+        dlg.exec_()
+
